8b2a2350dca6606c6fcfdf36cec9b5a8ca982574
ZARIF ubuntu <uzbzarif@gmail.com>
2021-09-24 18:44:20 +0900

    test 2

--- Variables and Types/test_1.py
+++ Variables and Types/test_1.py
@@ -3,9 +3,17 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 class Test(unittest.TestCase):
-    
+
     def test_begin_1(self):
         with patch('sys.stdout', new=StringIO()) as fake_output:
             import begin_1
             actual = fake_output.getvalue().strip()
             self.assertEqual(actual, 'Hello World')
+ 
+    def test_begin_2(self):
+        with patch('sys.stdout', new=StringIO()) as fake_output:
+            import begin_2
+            actual = fake_output.getvalue().strip()
+            output = 'Hello\nWorld'
+            self.assertEqual(actual, output)
+                       
